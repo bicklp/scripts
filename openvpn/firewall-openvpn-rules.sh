23556e2fde3851244b3049587a40d46b9d419d9f
bicklp <bicklp@gmail.com>
2015-11-09 16:27:10 +0000

    Update firewall-openvpn-rules.sh

--- openvpn/firewall-openvpn-rules.sh
+++ openvpn/firewall-openvpn-rules.sh
@@ -5,6 +5,6 @@
 #tun0 is OpenVPN Server
 #tun1 is PIA
 #
-iptables -t nat -A POSTROUTING -s 10.9.0.0/24 -o wlan0 -j SNAT --to-source 192.168.1.18 #[Local IP]
-iptables -t nat -A POSTROUTING -s 10.9.0.0/24 -o tun1 -j MASQUERADE
+iptables -t nat -A POSTROUTING -s 10.8.0.0/24 -o wlan0 -j SNAT --to-source 192.168.1.18 #[Local IP]
+iptables -t nat -A POSTROUTING -s 10.8.0.0/24 -o tun1 -j MASQUERADE
 #remove the square brackets from round the interface name
